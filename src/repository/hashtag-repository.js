fd8ca8231129d189189d65b3b606b09b894cb8bc
BiplawSingh <biplawgaurav@gmail.com>
2024-09-23 04:33:52 +0530

    Added crud repo and extended to other repos

--- src/repository/hashtag-repository.js
+++ src/repository/hashtag-repository.js
@@ -1,38 +1,16 @@
 import Hashtag from '../models/hashtags.js';
+import CrudRepository from './crud-repository.js';
 
-class HashtagRepository {
-    
-    async create(data) {
-        try {
-            const tag = await Hashtag.create(data);
-            return tag;
-        } catch (error) {
-            console.log(error);
-        }
+class HashtagRepository extends CrudRepository {
+
+    constructor() {
+        super(Hashtag);
     }
 
     async bulkCreate(data) {
         try {
             const tags = await Hashtag.insertMany(data);
             return tags;
-        } catch (error) {
-            console.log(error);
-        }
-    }
-    
-    async get(id) {
-        try {
-            const tag = await Hashtag.findById(id);
-            return tag; 
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
-    async destroy(id) {
-        try {
-            const response = await Hashtag.findByIdAndRemove(id);
-            return response;
         } catch (error) {
             console.log(error);
         }
--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,25 +1,12 @@
 import Tweet from '../models/tweet.js';
+import CrudRepository from './crud-repository.js';
 
-class TweetRepository {
-    
-    async create(data) {
-        try {
-            const tweet = await Tweet.create(data);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
+class TweetRepository extends CrudRepository {
+
+    constructor() {
+        super(Tweet);
     }
     
-    async get(id) {
-        try {
-            const tweet = await Tweet.findById(id);
-            return tweet; 
-        } catch (error) {
-            console.log(error);
-        }
-    }
-
     async getAll(offset, limit) {
         try {
             const tweet = await Tweet.find().skip(offset).limit(limit);
@@ -38,16 +25,6 @@ class TweetRepository {
         }
     }
 
-  
-
-    async destroy(id) {
-        try {
-            const tweet = await Tweet.findByIdAndRemove(id);
-            return tweet;
-        } catch (error) {
-            console.log(error);
-        }
-    }
 }
 
 export default TweetRepository;
