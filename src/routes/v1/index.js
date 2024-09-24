a85bd2bf5f2e9f400d22c9a61bbb3d0f6f108e68
BiplawSingh <biplawgaurav@gmail.com>
2024-09-24 21:30:25 +0530

    Added authentication middleware back on tweet

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -8,7 +8,7 @@ import { signup, login } from "../../controllers/auth-controller.js";
 import { authenticate } from "../../middlewares/authenticate.js";
 
 const router = express.Router();
-router.post("/tweets", createTweet);
+router.post("/tweets", authenticate, createTweet);
 router.get('/tweets/:id', getTweetWithComments);
 
 router.post("/likes/toggle", toggleLike);
